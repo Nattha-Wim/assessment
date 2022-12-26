9d72a9141003d7b90bfa6f0d19f774e7fc53de17
Mook <natthaporn.wim@gmail.com>
2022-12-26 16:22:10 +0700

    add graceful shutdown in server file

--- server.go
+++ server.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
 
@@ -24,16 +29,26 @@ func main() {
 		return false, nil
 	}))
 
-	e.POST("/expenses", expense.CreateExpense)
-	e.GET("/expenses/:id", expense.GetExpenseById)
+	e.GET("/", expense.HomeExpenses)
+	e.POST("/expenses", expense.CreateExpenses)
+	e.GET("/expenses/:id", expense.GetExpensesById)
 	e.GET("/expenses", expense.GetAllExpenses)
-
 	e.PUT("/expenses/:id", expense.UpdateExpenses)
 
-	log.Println("server start at :2565")
-	log.Fatal(e.Start(":2565"))
-	log.Println("bye bye")
+	go func() {
+		log.Println("server starting at :2565")
+		if err := e.Start(":2565"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
 
-	// fmt.Println("Please use server.go for main file")
-	// fmt.Println("start at port:", os.Getenv("PORT"))
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+	log.Println("shuting down.....")
+	if err := e.Shutdown(context.Background()); err != nil {
+		e.Logger.Fatal(err)
+	}
+	log.Println("Bye Bye")
+
 }
